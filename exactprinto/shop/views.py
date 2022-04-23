afaf5642ccb84154b27aa6d0719b9e960e941f37
abdulsamadirfan <abdulsamad141@outlook.com>
2022-04-24 01:24:07 +0500

    code push

--- exactprinto/shop/views.py
+++ exactprinto/shop/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from math import ceil
+
+
+
 # Create your tests here.
 def index(request):
-    params = {}
-    Products = Product.objects.all()
+    # Products = Product.objects.all()
+    
+    # n = len(Products)
+    # nSlides = n//4 +ceil((n/4) - (n//4))
+    # allprod = [[Products,range(1,nSlides), nSlides],#len(Products)
+    #            [Products,range(1,nSlides), nSlides]]
+
+    allprod  = []
+    catprod = Product.objects.values('category')
+    # print(catprod)
+    cats = {item['category'] for item in catprod}
+    # print(cats)
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nSlides = n//4 +ceil((n/4) - (n//4))
+        allprod.append([prod,range(1,nSlides), nSlides])
+
+
+
+    params = {"allprods":allprod}
+    # params = {"no_of slides": nSlides,"range": range(1,nSlides),"products" : Products}
     return render(request, 'shop/index.html',params)
 
 
